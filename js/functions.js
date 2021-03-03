0c256d136f2deadd375fd9d1b8f6115e0b546318
Houston Erekson <herekson@sanitarysupplycorp.com>
2021-03-03 10:07:42 -0700

    Feature Add

--- js/functions.js
+++ js/functions.js
@@ -3,88 +3,117 @@ var variables = {
         'inputName': 'Manufacturer',
         'inputType': 'selection',
         'selections': ['Papernet', 'Tork', 'vonDrehle'],
-        'inSpecTable': true
+        'nameOrder' : 0,
+        'inSpecTable': true,
+        'tableOrder' : 20
+    }, {
+        'inputName': 'Product Family',
+        'inputType': 'selection',
+        'selections': ['enMotion Style', 'Advanced', 'I-Notch', 'Opticore Style', 'Star-Notch', 'Transend', 'Universal', 'Xpressnap', 'Y-Notch', 'HyTech Ocean', 'Confidence'],
+        'nameOrder' : 1,
+        'inSpecTable': true,
+        'tableOrder' : 21
+    }, {
+        'inputName': 'Product Title',
+        'inputType': 'text',
+        'nameOrder' : 2,
+        'inSpecTable': false
     }, {
         'inputName': 'Category',
         'inputType': 'selection',
         'selections': ['Dispenser', 'Facial', 'Hand', 'Towels', 'Napkins', 'Toilet', 'Paper', 'Wipers'],
-        'inSpecTable': false
+        'nameOrder' : 3,
+        'inSpecTable': false,
+        'tableOrder' : 0
     }, {
-        'inputName': 'Brand/Model',
-        'inputType': 'selection',
-        'selections': ['enMotion Style', 'Advanced', 'I-Notch', 'Opticore Style', 'Star-Notch', 'Transend', 'Universal', 'Xpressnap', 'Y-Notch', 'HyTech Ocean', 'Confidence'],
-        'inSpecTable': true
-    }, {
-        'inputName': 'Product Name',
+        'inputName': 'Product Category',
         'inputType': 'selection',
         'selections': ['Center Pull', 'Multifold', 'Hardwound Roll', 'Toilet Paper', 'Facial Tissue', 'Napkin', 'Kitchen Roll', 'Singlefold', 'Windshield', 'Wiper'],
-        'inSpecTable': false
+        'nameOrder' : 3,
+        'inSpecTable': false,
+        'tableOrder' : 0
     }, {
         'inputName': 'Modifier',
         'inputType': 'selection',
         'selections': ['Antimicrobial', 'Bio Tech', 'Coreless', 'Cube Box', 'Dinner', 'Dissolvetech', 'Double Layer', 'Dry Tech', 'Electronic', 'Feather Soft', 'Feather Soft HC', 'Flex Core', 'Heavenly Soft', 'Heavenly Soft Superior', 'Heavy Duty', 'High Capacity', 'Jumbo', 'Kraft', 'Low Lint', 'Mechanical', 'Mini - Compact', 'Mini-Jumbo', 'Precious', 'Premium', 'Preserve HC', 'Preserve Plus DRC', 'Quarter Fold', 'Rectangular Box', 'ShopMax', 'Single Jumbo', 'Single Mini Jumbo', 'Smart Core', 'Split Core', 'Standard', 'Triple Down Facing', 'Twin Down Facing', 'Twin Front Facing', 'Twin Jumbo', 'Twin Mini Jumbo', '10 Inch'],
-        'inSpecTable': false
+        'nameOrder' : 4,
+        'inSpecTable': false,
+        'tableOrder' : 0
     }, {
         'inputName': 'Color',
         'inputType': 'selection',
         'selections': ['Black', 'Blue', 'Brown', 'Clear', 'Gray', 'Green', 'Matte Black', 'Matte White', 'Orange', 'Purple', 'Red', 'Smoke', 'White', 'Yellow'],
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Vendor MPN or SKU',
         'inputType': 'text',
-        'inSpecTable': true
+        'nameOrder' : 5,
+        'inSpecTable': true,
+        'tableOrder' : 22
     }, {
         'inputName': 'Vendor UPC',
         'inputType': 'text',
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 23
     }, {
-        'inputName': 'Unit of Measurement',
+        'inputName': 'Sold By',
         'inputType': 'selection',
         'selections': ['Case', 'Each'],
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Individually Wrapped',
         'inputType': 'selection',
         'selections': ['Yes', 'No'],
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Plys',
         'inputType': 'selection',
         'selections': ['1-Ply', '2-Ply', '3-Ply', '4-ply'],
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Qty per Case',
         'inputType': 'number',
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Sheets per Roll',
         'inputType': 'number',
-        'inSpecTable': true
+        'inSpecTable': true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Sheet Width (in)',
         'inputType': 'number',
         'inSpecTable': true,
-        'isMeasurement' : true
+        'isMeasurement' : true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Sheet Length (in)',
         'inputType': 'number',
         'inSpecTable': true,
-        'isMeasurement' : true
+        'isMeasurement' : true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Roll Length (ft)',
         'inputType': 'number',
         'inSpecTable': true,
-        'isMeasurement' : true
+        'isMeasurement' : true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Roll Diameter (in)',
         'inputType': 'number',
         'inSpecTable': true,
-        'isMeasurement' : true
+        'isMeasurement' : true,
+        'tableOrder' : 0
     }, {
         'inputName': 'Core Diameter (in)',
         'inputType': 'number',
         'inSpecTable': true,
-        'isMeasurement' : true
+        'isMeasurement' : true,
+        'tableOrder' : 0
     }]
 };
 
@@ -118,6 +147,7 @@ function buildInputOptions(selectedType) {
     inputOptionsContainer.innerHTML = "";
     var ptype = variables[selectedType];
     ptype.forEach(function(item) {
+    	console.debug(item);
         var inputDiv = document.createElement('div'); //create a div for the input field
         inputDiv.className = 'mb-3'; //add classed to the div
         inputOptionsContainer.appendChild(inputDiv); //add div to productspecifications
@@ -125,7 +155,7 @@ function buildInputOptions(selectedType) {
         switch (item.inputType) {
             case 'number':
                 var inputField = document.createElement('input');
-                inputField.id = inputNiceName; //give the input element a unique ID
+                inputField.id = inputNiceName; //give the select element a unique ID
                 inputField.className = 'inputfield form-control form-control-sm';
                 inputField.type = 'number';
                 inputField.placeholder = item.inputName;
@@ -134,7 +164,7 @@ function buildInputOptions(selectedType) {
                 break;
             case 'text':
                 var inputField = document.createElement('input');
-                inputField.id = inputNiceName; //give the input element a unique ID
+                inputField.id = inputNiceName; //give the select element a unique ID
                 inputField.className = 'inputfield form-control form-control-sm';
                 inputField.type = 'text';
                 inputField.placeholder = item.inputName;
@@ -152,13 +182,15 @@ function buildInputOptions(selectedType) {
                 placeholder.text = '- Select ' + item.inputName + '  -';
                 placeholder.value = '';
                 placeholder.setAttribute('selected', '');
-                placeholder.setAttribute('disabled', '');
+                // placeholder.setAttribute('disabled', ''); //Disable first option
                 inputField.add(placeholder);
                 buildOptions(item.selections, inputNiceName);
                 break;
 
         }
+        inputField.setAttribute('nameOrder', item.nameOrder);
         inputField.setAttribute('inSpecTable', item.inSpecTable);
+        inputField.setAttribute('tableOrder', item.tableOrder);
     });
 }
 
@@ -177,11 +209,12 @@ function buildContent() {
     var outputHTML = [];
     var preview = document.getElementById('preview');
     var content = tinymce.get("content").getContent();
-    var productName = document.createElement('h3');
-    productName.innerHTML = document.getElementById('productName').value;
     var output = document.getElementById("output");
     var tableInputs = document.getElementById('productspecifications').getElementsByClassName('inputfield'); //get the input fields
     var tableInputsArray = Array.prototype.slice.call(tableInputs); //slice the inputs into individual HTMLelement blocks
+    tableInputsArray.sort((a, b) => (a.getAttribute('tableorder') > b.getAttribute('tableorder') ) ? 1 : -1 ) //sort the items by tableOrder
+    
+    // Build information table
     let table = document.createElement('table'); //create a table
     table.className = "table table-striped"; //give the table a classname
     tableInputsArray.forEach(function(item) {
@@ -196,6 +229,24 @@ function buildContent() {
         	//console.log(item.id + ' failed');
         }
     });
+
+    // Build the Product Name
+    var productName = document.createElement('h3');
+    var productNameArray = [];
+    tableInputsArray.sort((a, b) => (a.getAttribute('nameorder') > b.getAttribute('nameorder') ) ? 1 : -1 ) //sort the items by nameOrder
+   
+    tableInputsArray.forEach(function(item) {
+
+    	var nameOrderAttribute = item.getAttribute('nameOrder');
+    	if( nameOrderAttribute !== 'undefined' ){
+    		productNameArray.push( item.value );
+    		console.log(item.id + ' Name Order = ' + item.getAttribute('nameOrder'))
+    	}
+
+    });
+
+    productName.textContent = (productNameArray.join(', ') );
+
     outputHTML.push(productName.outerHTML);
     outputHTML.push(content);
     outputHTML.push('<h5><strong>Product Specifications</strong></h5>');
